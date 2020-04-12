ed00e32e096ec99732bbe4b27a185d0a7ec673e5
Ubuntu <ubuntu@ubuntu.hitronhub.home>
2020-04-12 02:34:03 +0000

    init src commit

---
+++ src/trials.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Zaber Motion ASR100B120B-T3 Stage Trials
+========================================
+Contributors: Christian Sargusingh
+Date: 2020-04-11
+Repository: https://github.com/cSDes1gn/stage-trials
+README available in repository root
+Version: 1.0
+
+Using X-MCB2 controller
+
+Dependencies
+------------
+>>> from zaber_motion import Library 
+>>> from zaber_motion.ascii import Connection
+ 
+Copyright © 2020 Incuvers, Inc - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+"""
+from zaber_motion import Library 
+from zaber_motion.ascii import Connection
+
+Library.toggle_device_db_store(True)
+
+
+# open serial port
+with Connection.open_serial_port("/dev/ttyACM0") as conn:
+    print("Found {} devices".format(conn.detect_devices()))
